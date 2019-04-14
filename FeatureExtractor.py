e3394435b1e519cc7de1f1cbb534ca695c2fbe5e
vtanguturi <vtanguturi3@gatech.edu>
2019-04-14 16:01:32 -0400

    Added mel-frequency-cepstral coefficients as a feature

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -22,6 +22,7 @@ class FeatureExtractor(object):
         features = []
         features.extend(self.get_energy(subclips))
         features.extend(self.get_zero_crossing(subclips))
+        features.extend(self.get_mel_freq_cepstral_coeffs(subclips))
         return features
 
     def get_energy(self, subclips):
@@ -41,3 +42,41 @@ class FeatureExtractor(object):
             count = count + 1
 
         return [np.mean(zcs), np.std(zcs)]
+
+    def get_mel_freq_cepstral_coeffs(self, subclips):
+        mfccs = []
+        for sc in subclips:
+            si_k = np.fft.fft(sc, n=512)
+            pi_k = (1/len(si_k)) * (np.abs(si_k) ** 2)
+            pi_k = pi_k[:257]
+            fil_banks = self.compute_filterbank()
+            fil_energies = np.sum(pi_k * fil_banks, axis=1)
+            log_energies = np.log10(fil_energies)
+            cepstral_coeff = sp.fftpack.dct(log_energies, norm='ortho')
+            mfcc = cepstral_coeff[:12]
+            mfccs.append(mfcc)
+        return mfccs
+
+    def compute_filterbank(self, nfft=512, nfilt=26):
+        lowmel = self.convert_freq_to_mel(0)
+        highmel = self.convert_freq_to_mel(sr/2)
+
+        melpoints = np.linspace(lowmel,highmel,nfilt+2)
+        # our points are in Hz, but we use fft bins, so we have to convert
+        #  from Hz to fft bin number
+        bin_number = np.floor((nfft+1)*self.convert_mel_to_freq(melpoints)/sr)
+
+        fbank = np.zeros([nfilt,nfft//2+1])
+        for j in range(0,nfilt):
+            for i in range(int(bin_number[j]), int(bin_number[j+1])):
+                fbank[j,i] = (i - bin_number[j]) / (bin_number[j+1]-bin_number[j])
+            for i in range(int(bin_number[j+1]), int(bin_number[j+2])):
+                fbank[j,i] = (bin_number[j+2]-i) / (bin_number[j+2]-bin_number[j+1])
+        return fbank
+
+    def convert_freq_to_mel(self, frequency):
+        return 2595 * np.log10(1+frequency/700.)
+
+    def convert_mel_to_freq(self, mel):
+        return 700 * np.exp((mel/1125.) - 1)
+
